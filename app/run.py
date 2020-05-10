44736002b05edd5e672ddb1ebd01672cc5f4180e
evertonweyand <evertonweyand@gmail.com>
2020-05-09 22:00:18 -0300

    Ajustes nos comentários

--- app/run.py
+++ app/run.py
@@ -2,19 +2,23 @@ import random
 from app.jogador import Jogador
 from app.time import Time
 
+"""
+Cria uma lista vazia para adicionar os jogadores
+"""
 jogadores = []
 """
-Criar a lista com todos os jogadores, depois esta lista virá do banco de dados.
+Criar a lista com todos os jogadores.
+Depois esta lista virá do banco de dados.
 """
 for x in range(10):
     if x > 7:
-        nivel = 3
+        nivel = 3 # Os jogadores 7 e 8 serão os 2 goleiros
     elif x % 2 == 0:
-        nivel = 1
+        nivel = 1 # Os jogadores pares serão de nível 1, bola cheia
     else:
-        nivel = 2
+        nivel = 2 # Os jogadores ímpares serão de nível 2, bola murcha
 
-    jogadores.append(Jogador('Everton {} '.format(x), nivel, 'S'))
+    jogadores.append(Jogador('Jogador {} '.format(x), nivel, 'S'))
 
 times = Time(jogadores)
 times.imprimir_times()
